702250e0b2eceb4b40347105f797aa8ca7c5d7c3
Aorg <249753396@qq.com>
2024-01-09 12:01:02 +0800

    Update run_gradio.py

--- langchain/run_gradio.py
+++ langchain/run_gradio.py
@@ -4,7 +4,7 @@ os.system("pip install -U pypdf;pip install langchain==0.0.292")
 os.system("python -m pip install --upgrade pip;pip install modelscope==1.9.5;pip install transformers==4.35.2;pip install streamlit==1.24.0;pip install sentencepiece==0.1.99;pip install accelerate==0.24.1;pip install chromadb==0.4.15;pip install sentence-transformers==2.2.2;pip install unstructured==0.10.30;pip install markdown==3.3.7")
 os.system("git clone https://gitee.com/yzy0612/nltk_data.git  --branch gh-pages;cd nltk_data;mv packages/*  ./;cd tokenizers;unzip punkt.zip;cd ../taggers;unzip averaged_perceptron_tagger.zip")
 from openxlab.model import download
-download(model_repo='OpenLMLab/InternLM-chat-7b',  model_name='InternLM-chat-7b', output='/home/xlab-app-center/OpenLMLab/InternLM-chat-7b')
+download(model_repo='OpenLMLab/InternLM-chat-7b',  model_name='InternLM-chat-7b', output='/home/xlab-app-center/')
 # 设置环境变量
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 # 下载模型
@@ -25,7 +25,7 @@ def load_chain():
     embeddings = HuggingFaceEmbeddings(model_name="/home/xlab-app-center/sentence-transformer")
     # 向量数据库持久化路径
     persist_directory = 'math_base'
-    model_path='/home/xlab-app-center/OpenLMLab/InternLM-chat-7b'
+    model_path='OpenLMLab/InternLM-chat-7b'
 
     # 加载数据库
     vectordb = Chroma(

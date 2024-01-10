cb7e340066322464cda7e915b71ab7ca658ba5c4
Aorg <249753396@qq.com>
2024-01-10 10:52:18 +0800

    Update run_gradio.py

--- langchain/run_gradio.py
+++ langchain/run_gradio.py
@@ -14,8 +14,10 @@ os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 os.system('pip install -U huggingface_hub; huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /home/xlab-app-center/sentence-transformer')
 os.system("pip install -r /home/xlab-app-center/langchain/requirements.txt")
 os.system("pip install opencv-python;pip install pytesseract;pip install python-docx;pip install -U pypdf;pip install chromadb==0.3.29")#pip install chromadb==0.3.29;
+
 os.system("python langchain/create_db.py")
 # 导入必要的库
+print(os.listdir('/home/xlab-app-center/')
 print(os.getcwd())
 import gradio as gr
 from langchain.vectorstores import Chroma

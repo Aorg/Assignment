4c5e63cc8e6e5117131d8001f470cc8c8b725a51
unknown <249753396@qq.com>
2024-01-10 15:13:03 +0800

    更换成2b模型

--- langchain/LLM.py
+++ langchain/LLM.py
@@ -28,11 +28,11 @@ class InternLM_LLM(LLM):
         print("正在从本地加载模型...")
         # self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
         # self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, torch_dtype=torch.bfloat16)#load_in_4bit=True
-        tokenizer = LlamaTokenizer.from_pretrained('IEITYuan/Yuan2-2B-hf', add_eos_token=False, add_bos_token=False, eos_token='<eod>')
+        tokenizer = LlamaTokenizer.from_pretrained(model_path, add_eos_token=False, add_bos_token=False, eos_token='<eod>')
         tokenizer.add_tokens(['<sep>', '<pad>', '<mask>', '<predict>', '<FIM_SUFFIX>', '<FIM_PREFIX>', '<FIM_MIDDLE>','<commit_before>','<commit_msg>','<commit_after>','<jupyter_start>','<jupyter_text>','<jupyter_code>','<jupyter_output>','<empty_output>'], special_tokens=True)
 
         print("Creat model...")
-        model = AutoModelForCausalLM.from_pretrained('IEITYuan/Yuan2-2B-hf',torch_dtype=torch.bfloat16, trust_remote_code=True)
+        model = AutoModelForCausalLM.from_pretrained(model_path,torch_dtype=torch.bfloat16, trust_remote_code=True)
 
         self.model = self.model.eval
         print("完成本地模型的加载")
